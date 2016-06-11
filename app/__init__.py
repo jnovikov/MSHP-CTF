66104457e96537da4cba546d7100261bff65925f
Ivan Novikov <johnnovikov0@gmail.com>
2016-06-11 04:05:13 +0300

    Created beta-version of project

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CsrfProtect
 
+
 app = Flask(__name__)
 
 app.config.from_object('config')
@@ -10,7 +11,8 @@ csrf = CsrfProtect()
 CsrfProtect(app)
 csrf.init_app(app)
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
 
 
 from app.views import base_views
---
+++ app/controllers/team_controller.py
@@ -0,0 +1,44 @@
+from app.models.db_models import Team
+from app import db
+
+
+def get_team(_name):
+    team = Team.query.filter_by(name=_name).first()
+    return team
+
+
+def get_team_id(_name):
+    team = Team.query.filter_by(name=_name).first()
+    if team is not None:
+        return team.id
+
+
+def create_team(_name):
+    team = get_team(_name)
+    if team is not None:
+        return 'Team already exist'
+    print('Adding team', _name)
+    db.session.add(Team(_name))
+    db.session.commit()
+    return True
+
+
+def get_team_solved_tasks(_id):
+    team = Team.query.filter_by(id=_id).first()
+    solved = team.solved.split()
+    solved = list(map(int, solved))
+    return solved
+
+
+def solve_task(_id, task_id, task_score):
+    team = Team.query.filter_by(id=_id).first()
+    if team is None:
+        return 'Team not exist'
+    team.solve_task(task_id, task_score)
+    db.session.commit()
+    return True
+
+
+def get_team_scores():
+    team = Team.query.group_by(Team.score).all()
+    return team[::-1]
