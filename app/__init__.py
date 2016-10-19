474916e969e88818ce0d2055ccf15db2af40eee3
Ivan Novikov <johnnovikov0@gmail.com>
2016-10-20 02:28:52 +0300

    АДМИНКА ПОДЪЕХАЛА

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,15 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CsrfProtect
+from flask_admin import Admin
 
-app = Flask(__name__)
+
+class FlaskWithTemplatesAutoReload(Flask):
+    def create_jinja_environment(self):
+        self.config['TEMPLATES_AUTO_RELOAD'] = True
+        return Flask.create_jinja_environment(self)
+
+app = FlaskWithTemplatesAutoReload(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
 
 app.config.from_object('config')
@@ -14,10 +21,10 @@ CsrfProtect(app)
 csrf.init_app(app)
 
 db = SQLAlchemy(app)
-
+admin = Admin(app, name='CTF', template_mode='bootstrap3')
 db.create_all()
 
 from app.views.base_views import view
-
+from app.views import admin_view
 app.register_blueprint(view)
 
---
+++ app/views/admin_view.py
@@ -0,0 +1,21 @@
+from flask import redirect
+from flask import session
+
+from app import app, admin,db
+from app.models.db_models import User,Task
+from flask_admin.contrib.sqla import ModelView
+
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        print(session)
+        return session['login'] == 'admin'
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        print('why??')
+        return redirect('/')
+
+
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Task, db.session))
