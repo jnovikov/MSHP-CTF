9bff332722257933713a26b8f08682ee84643039
Ivan Novikov <johnnovikov0@gmail.com>
2017-10-23 00:20:25 +0300

    Супер админка

--- app/views/admin_view.py
+++ app/views/admin_view.py
@@ -7,6 +7,8 @@ from flask_admin.contrib.sqla import ModelView
 
 
 class MyModelView(ModelView):
+    column_editable_list = []
+
     def is_accessible(self):
         print(session)
         return session['login'] == 'admin'
@@ -17,8 +19,26 @@ class MyModelView(ModelView):
         return redirect('/')
 
 
+class ContestView(MyModelView):
+    create_modal = True
+    edit_modal = True
+    column_editable_list = ['name', 'active', 'description']
+
+
+class TaskModelView(MyModelView):
+    create_modal = True
+    edit_modal = True
+    column_editable_list = ['name', 'cost', 'category', 'flag', 'file', 'active']
+    column_searchable_list = column_editable_list
+
+
+class ContestTaskView(MyModelView):
+    create_modal = True
+    edit_modal = True
+
+
 admin.add_view(MyModelView(User, db.session))
-admin.add_view(MyModelView(Task, db.session))
+admin.add_view(TaskModelView(Task, db.session))
 admin.add_view(MyModelView(SolvedTask, db.session))
-admin.add_view(MyModelView(Contest, db.session))
-admin.add_view(MyModelView(ContestTask, db.session))
+admin.add_view(ContestView(Contest, db.session))
+admin.add_view(ContestTaskView(ContestTask, db.session))
